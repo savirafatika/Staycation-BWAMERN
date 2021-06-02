1d877454b688a24006d9dd59f8f0280096032cd8
Savira Fatika <74229372+savirafatika@users.noreply.github.com>
2021-06-02 21:24:05 +0700

    add styling index.scss component inputNumber for DetailsPage and testing component index.test.js

--- src/elements/Form/InputNumber/index.js
+++ src/elements/Form/InputNumber/index.js
@@ -6,7 +6,7 @@ import './index.scss'
 
 export default function Number(props) {
 
-   const { value, placeholder, name, min, max, prefix, suffix } = props
+   const { value, placeholder, name, min, max, prefix, suffix, isSuffixPlural } = props
    const [ InputValue, setInputValue ] = useState(`${prefix}${value}${suffix}`) 
 
    const onChange = e => {
@@ -24,7 +24,7 @@ export default function Number(props) {
                value: +value
             }
          })
-         setInputValue(`${prefix}${value}${suffix}`)
+         setInputValue(`${prefix}${value}${suffix}${isSuffixPlural && value > 1 ? "s" : ""}`)
       }
    }
 
@@ -86,6 +86,7 @@ Number.defaultProps = {
 Number.propTypes = {
    value: propTypes.oneOfType([propTypes.string, propTypes.number]),
    onChange: propTypes.func,
+   isSuffixPlural: propTypes.bool,
    placeholder: propTypes.string,
    outerClassName: propTypes.string
 }
