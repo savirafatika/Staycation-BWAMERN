004ee0915448d6f8e3b59239c05c2f7778263a32
Savira Fatika <74229372+savirafatika@users.noreply.github.com>
2021-06-08 22:02:48 +0700

    add Page Details

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import 'assets/scss/style.scss';
+
 import LandingPage from 'pages/LandingPage';
+import DetailsPage from 'pages/DetailsPage';
+import Example from 'pages/Example';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Route exact path="/" component={LandingPage}></Route>
+        <Route exact path="/properties/:id" component={DetailsPage}></Route>
+        <Route path="/example" component={Example}></Route>
       </Router>
     </div>
   );
